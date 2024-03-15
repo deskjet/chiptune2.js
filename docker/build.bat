4d6e9baaada8482e6b60de764ced82b4eb4b544d
DrSnuggles <snugglesdr@hotmail.com>
2024-03-15 17:02:54 +0100

    Fixed versioned path

--- docker/build.bat
+++ docker/build.bat
@@ -1,6 +1,6 @@
 docker build --build-arg BASE=https://lib.openmpt.org/files/libopenmpt/src/ --build-arg FILE=libopenmpt-0.7.4+release -t emscripten:libopenmpt .
 docker create --name mpt emscripten:libopenmpt
-docker cp mpt:/src/libopenmpt-0.7.4+release/bin/libopenmpt.js ../libopenmpt.worklet.js
+docker cp mpt:/src/libopenmpt/bin/libopenmpt.js ../libopenmpt.worklet.js
 docker rm mpt
 docker rmi emscripten:libopenmpt
 docker rmi emscripten/emsdk:latest
